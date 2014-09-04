e476b9f1d31730c7df1fde8f61d4e0c468fe56dc
Emil Pirfalt <emil.pirfalt@gmail.com>
2014-09-04 17:22:30 +0200

    Move script and style to a single file each.

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+function insertAfter(referenceNode, newNode) {
+  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+function main() {
+  console.log('Check back in a while to see what this playground is up too :)')
+}
+main()
