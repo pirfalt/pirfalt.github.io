0ea3a1b536f0f7dc88b8d689b2096c0e7097fe87
Emil Pirfalt <emil.pirfalt@gmail.com>
2014-09-12 13:13:18 +0200

    update gulpfile.js. added build and livereload

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,19 +1,64 @@
 var gulp = require('gulp')
+var gutil = require('gulp-util')
+var filter = require('gulp-filter')
+var concat = require('gulp-concat')
 var livereload = require('gulp-livereload')
+var cached = require('gulp-cached')
+var markdown = require('gulp-markdown')
 var ecstatic = require('ecstatic')
 var http = require('http')
+var through = require('through2')
 
-var fileglob = ['*','2010/**/*', 'assets/**/*']
+var globs = {
+  src: 'src/**/*',
+  out: 'build/**/*',
+  md: '**/*.md',
+  js: '**/*.js',
+  html: '**/*.html'
+}
 
-gulp.task('watch', ['serve'], function() {
-  livereload.listen()
-  gulp.watch(fileglob).on('change', livereload.changed)
+var dirs = {
+  src: 'src',
+  out: 'build'
+}
+
+var lrscript = new Buffer('<script>document.write(\'<script src="http://\' + (location.host || \'localhost\').split(\':\')[0] + \':35729/livereload.js?snipver=1"></\' + \'script>\')</script>')
+var live = true
+
+gulp.task('build', function() {
+  var md = filter(globs.md)
+  var html = filter(globs.html)
+
+  return gulp.src([globs.src])
+    .pipe(cached(dirs.out))
+    .pipe(md)
+      .pipe(markdown())
+      .pipe(md.restore())
+    .pipe(html)
+      .pipe(live
+        ? through.obj(function transform(file, enc, cb) {
+          file.contents = Buffer.concat([file.contents, lrscript])
+          this.push(file)
+          cb()
+        })
+        : gutil.noop())
+      .pipe(html.restore())
+    .pipe(gulp.dest(dirs.out))
 })
 
-gulp.task('serve', function(next) {
-  http.createServer(ecstatic({ root: __dirname })).listen(8080, function(err) {
+
+gulp.task('server', ['build'], function(next) {
+  return http.createServer(ecstatic({ root: __dirname + '/' + dirs.out })).listen(8080, function(err) {
     if (err) throw err
-    console.log('Listening on :8080')
+    gutil.log('Static fileserver Listening on: 8080')
     next()
   })
+})
+
+
+gulp.task('watch', ['server'], function(next) {
+  livereload.listen()
+  gulp.watch([globs.out], livereload.changed)
+
+  gulp.watch([globs.src], ['build'])
 })
